35531210629f5b7c63b44a4344ef7d901219f42f
dongkyunKim <kyun010713@gmail.com>
2024-06-20 17:55:02 +0900

    feat: controller 추가

--- src/main/java/com/example/oauth2_study/controller/MyController.java
+++ src/main/java/com/example/oauth2_study/controller/MyController.java
@@ -5,12 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class MainController {
-
-    @GetMapping("/")
+public class MyController {
+    @GetMapping("/my")
     @ResponseBody
-    public String mainAPI() {
-
-        return "main route";
+    public String myAPI() {
+        return "my route";
     }
 }
