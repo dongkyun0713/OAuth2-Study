621d4c83dacbe704ebfa4eab4c66ce7789fa6ea0
dongkyunKim <kyun010713@gmail.com>
2024-06-25 01:53:29 +0900

    style: 포맷팅 변경

--- src/main/java/com/example/oauth2_study/repository/UserRepository.java
+++ src/main/java/com/example/oauth2_study/repository/UserRepository.java
@@ -1,9 +1,8 @@
 package com.example.oauth2_study.repository;
 
-import com.example.oauth2_study.entity.UserEntity;
+import com.example.oauth2_study.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
-    UserEntity findByUsername(String username);
-
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
 }
